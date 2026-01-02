86a89fd913ce4f35428b64a5fd4437b116e7eb65
Amir <wild4bs@gmail.com>
2026-01-02 03:53:31 +0300

    edits

--- components/ui/dashboard/DynamicSection/InputField.tsx
+++
@@ -1,9 +0,0 @@
-import { Label } from "../../label";
-import { Input } from "../Input";
-
-export const InputField = ({ label, value, onChange }: any) => (
-  <div className="space-y-1">
-    <Label className="capitalize">{label}</Label>
-    <Input value={value} onChange={(e) => onChange(e.target.value)} />
-  </div>
-);
